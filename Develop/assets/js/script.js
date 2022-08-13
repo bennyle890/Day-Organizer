cc969b700b2abfc6a17d4d8422e7d7af9f53a1f3
Benny Le <benny.le890@gmail.com>
2022-08-13 03:00:02 -0400

    added time blocks

--- Develop/assets/js/script.js
+++ Develop/assets/js/script.js
@@ -1,3 +1,6 @@
+document.onload = getValueText();
+
+// Date and Time
 function updateClock(){
     var now = new Date();
     var dname = now.getDay(),
@@ -35,3 +38,50 @@ function initClock() {
     updateClock();
     window.setInterval("updateClock()", 1);
 }
+
+function getValueText() {
+    if (localStorage.key !== null) {
+        var hr8 = localStorage.getItem('hr-8');
+        $('#hr-8').children('textarea').val(hr8);
+
+        var hr9 = localStorage.getItem('hr-9');
+        $('#hr-9').children('textarea').val(hr9);
+
+        var hr10 = localStorage.getItem('hr-10');
+        $('#hr-10').children('textarea').val(hr10);
+
+        var hr11 = localStorage.getItem('hr-11');
+        $('#hr-11').children('textarea').val(hr11);
+
+        var hr12 = localStorage.getItem('hr-12');
+        $('#hr-12').children('textarea').val(hr12);
+        
+        var hr1 = localStorage.getItem('hr-1');
+        $('#hr-1').children('textarea').val(hr1);
+
+        var hr2 = localStorage.getItem('hr-2');
+        $('#hr-2').children('textarea').val(hr2);
+
+        var hr3 = localStorage.getItem('hr-3');
+        $('#hr-3').children('textarea').val(hr3);
+
+        var hr4 = localStorage.getItem('hr-4');
+        $('#hr-4').children('textarea').val(hr4);
+
+        var hr5 = localStorage.getItem('hr-5');
+        $('#hr-5').children('textarea').val(hr5);
+    }
+}
+
+$('.save-button').on('click', function(event) {
+    event.preventDefault();
+    var element = event.target;
+
+    if (element.matches('button') === true) {
+        var hourBlock = $(element).parent().attr('id');
+        var valueText = $(element).siblings('textarea').val().trim();
+
+        localStorage.setItem(hourBlock, valueText);
+        getValueText();
+    }
+})

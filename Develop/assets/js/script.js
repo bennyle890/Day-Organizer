46614f1192bea03fba2dd86e2a03ddb2d9aa3c7c
Benny Le <benny.le890@gmail.com>
2022-08-16 00:50:33 -0400

    removed old time block and added new time with color change

--- Develop/assets/js/script.js
+++ Develop/assets/js/script.js
@@ -1,41 +1,31 @@
-// Date and Time
-function updateClock(){
-    var now = new Date();
-    var dname = now.getDay(),
-        mo = now.getMonth(),
-        dnum = now.getDate(),
-        yr = now.getFullYear(),
-        hou = now.getHours(),
-        min = now.getMinutes(),
-        sec = now.getSeconds(),
-        pe = "AM";
+$(document).ready(function () {
+    // time variables
+    var currentHour = new Date().getHours();
+    var currentDateAndTime = Date(Date.now());
+    $("#currentDay").text(currentDateAndTime);
+    
 
-        if(hou == 0){
-            hou = 12;
-        }
-        if(hou >12){
-            hou = hou - 12;
-            pe = "PM";
-        }
+    document.onload = updateTime();
+    document.onload = getValueText();
 
-        Number.prototype.pad = function(digits){
-            for(var n = this.toString(); n.length < digits; n = 0 + n);
-            return n; 
-        }
+    // loops over time-blocks to set past, present, or future classes which determines css styling
+    function updateTime() {
+        
+        $('.time-block').each(function () {
+            var hour = parseInt($(this).attr('id').split('-')[1])
+            // console.log(hour);
+            if (hour < currentHour) {
+                $(this).addClass('past');
+            } else if (hour === currentHour) {
+                $(this).removeClass('past');
+                $(this).addClass('present');
+            } else {
+                $(this).addClass('future');
+            }
+        })
+    }
+})
 
-        var months = ["January", "Februrary", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-        var week = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-        var ids = ["dayname", "month", "daynum", "year", "hour", "minutes", "seconds", "period"];
-        var values = [week[dname], months[mo], dnum.pad(2), yr, hou.pad(2), min.pad(2), sec.pad(2), pe];
-        for(var i = 0; i < ids.length; i++)
-        document.getElementById(ids[i]).firstChild.nodeValue = values[i];
-
-}
-
-function initClock() {
-    updateClock();
-    window.setInterval("updateClock()", 1);
-}
 
 
 function getValueText() {
